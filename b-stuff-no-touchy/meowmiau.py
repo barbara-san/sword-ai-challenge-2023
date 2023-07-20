3ca901661bf4b7fd0472cebd535deabeb61466af
Barbara Santos <blsbls03@gmail.com>
2023-07-20 16:17:12 +0100

    Update meowmiau.py

--- b-stuff-no-touchy/meowmiau.py
+++ b-stuff-no-touchy/meowmiau.py
@@ -1,6 +1,6 @@
-import streamlit as st
 import os
-import langchain
+import streamlit as st
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import AzureChatOpenAI
 from langchain.schema import HumanMessage
 
@@ -13,18 +13,37 @@ os.environ["OPENAI_API_BASE"] = AZURE_OPENAI_ENDPOINT
 os.environ["OPENAI_API_VERSION"] = OPENAI_API_VERSION
 os.environ["OPENAI_API_TYPE"] = "azure"
 
+''' chatgpt = AzureChatOpenAI(deployment_name="gpt35-team-3-0301")
+
+input_text = "How to be happy?"
+output_text = chatgpt([HumanMessage(content=input_text)]).content '''
 
 
-st.title('🦜🔗 Quickstart App')
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text="", display_method='markdown'):
+        self.container = container
+        self.text = initial_text
+        self.display_method = display_method
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token + "/"
+        display_function = getattr(self.container, self.display_method, None)
+        if display_function is not None:
+            display_function(self.text)
+        else:
+            raise ValueError(f"Invalid display_method: {self.display_method}")
 
-def generate_response(input_text):
-  llm = AzureChatOpenAI(deployment_name="gpt35-team-3-0301")
-  st.info(llm([HumanMessage(content=input_text)]).content)
+query = st.text_input("input your query", value="Tell me a joke")
+ask_button = st.button("ask")
 
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if submitted:
-    generate_response(text)
+st.markdown("### streaming box")
+chat_box = st.empty()
+stream_handler = StreamHandler(chat_box, display_method='write')
+chat = AzureChatOpenAI(max_tokens=100, streaming=True, callbacks=[stream_handler])
+
+st.markdown("### together box")
+
+if query and ask_button:
+    response = chat([HumanMessage(content=query)])
+    llm_response = response.content
+    st.markdown(llm_response)
